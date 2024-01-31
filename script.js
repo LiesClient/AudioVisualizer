bc00e36d5e802e3ce64603ed5b70b05e4571d6a9
lyinglint <77509842+LiesClient@users.noreply.github.com>
2024-01-31 10:34:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -132,7 +132,7 @@ function loop() {
   for (let i = 0; i < dataArray.length; i++) {
     let x = i / (dataArray.length + 0.5);
     let y = Math.max(dataArray[i] / 255, 0.01);
-    rightPanel.fillRect(x, (y * 0.9) - 0.05, (1.5 / dataArray.length), 0.1);
+    rightPanel.fillRect(x, (y * 0.9), (1.5 / dataArray.length), 0.1);
   }
 
   requestAnimationFrame(loop);
