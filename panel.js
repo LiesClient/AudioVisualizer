b699ae0fa624aa5014b1f8b4f80490c13789c6d7
lyinglint <77509842+LiesClient@users.noreply.github.com>
2024-02-03 11:31:16 -0500

    Update panel.js

--- panel.js
+++ panel.js
@@ -7,17 +7,20 @@ class Panel {
   width;
   height;
   maxZ;
+  vert = true;
 
   constructor (
     top,
     bottom,
     zFront,
-    zBack
+    zBack,
+    vert = true,
   ) {
     this.top = top;
     this.bot = bottom;
     this.zFr = zFront;
     this.zBa = zBack;
+    this.vert = vert;
   }
 
   setContext(ctx) {
@@ -31,7 +34,7 @@ class Panel {
   }
 
   #calculateZ(x, y) {
-    return lerp(this.zBa, this.zFr, x);
+    return lerp(this.zBa, this.zFr, this.vert ? x : y);
   }
   // (x, y) on panel to (ox, oy) to put on screen
   // (x, y) is the point on the panel
@@ -39,7 +42,7 @@ class Panel {
   
   translatePoint(x, y) {
     let z = this.#calculateZ(x, y);
-    let ox = this.top.x;
+    let ox = lerp(this.bot.x, this.top.x, x);
     let oy = lerp(this.bot.y, this.top.y, y);
     return this.from3d(ox, oy, z);
   }
@@ -74,6 +77,27 @@ class Panel {
     this.#rect(x, y, width, height);
     ctx.fill();
   }
+
+  rotatedSquare(x, y, width, angle) {
+    let addVec = rotateVec(norm({ x: 1, y: 1 }), angle);
+    let mag = width / 2;
+    let tl = this.translatePoint(x + addVec.x * mag, y + addVec.y * mag);
+    addVec = { x: -addVec.y, y: addVec.x };
+    let tr = this.translatePoint(x + addVec.x * mag, y + addVec.y * mag);
+    addVec = { x: -addVec.y, y: addVec.x };
+    let br = this.translatePoint(x + addVec.x * mag, y + addVec.y * mag);
+    addVec = { x: -addVec.y, y: addVec.x };
+    let bl = this.translatePoint(x + addVec.x * mag, y + addVec.y * mag);
+
+    ctx.beginPath();
+    ctx.moveTo(tl.x, tl.y);
+    ctx.lineTo(tr.x, tr.y);
+    ctx.lineTo(br.x, br.y);
+    ctx.lineTo(bl.x, bl.y);
+    ctx.lineTo(tl.x, tl.y);
+
+    return ctx;
+  }
 }
 
 function lerp(a, b, t) {
@@ -83,4 +107,11 @@ function lerp(a, b, t) {
 function norm(vec) {
   let mag = Math.sqrt(vec.x * vec.x + vec.y * vec.y);
   return { x: vec.x / mag, y: vec.y / mag };
+}
+
+function rotateVec(v, ang) {
+  return {
+    x: Math.cos(ang) * v.x - Math.sin(ang) * v.y,
+    y: Math.sin(ang) * v.x + Math.cos(ang) * v.y,
+  }
 }
